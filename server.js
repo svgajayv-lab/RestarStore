9247c9862a356a9dcc50ba083593391eeea36ed8
Natalio <svgajayv@gmail.com>
2026-05-13 20:53:27 -0500

    Fix Mongo timeout

--- server.js
+++ server.js
@@ -1,7 +1,28 @@
 require("dotenv").config();
+
 const mongoose = require("mongoose");
-const History = require("./models/History");
-mongoose.connect(process.env.MONGO_URI)
+
+mongoose.set("bufferCommands", false);
+
+mongoose.connect(process.env.MONGO_URI, {
+
+    useNewUrlParser: true,
+
+    useUnifiedTopology: true
+
+})
+
+.then(() => {
+
+    console.log("🔥 MongoDB conectado");
+
+})
+
+.catch((err) => {
+
+    console.log(err);
+
+});
 
 .then(() => {
 
