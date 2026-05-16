c601932f03ca90aee23ef5fcb71aca3d3b88cf73
Natalio <svgajayv@gmail.com>
2026-05-16 08:33:51 -0500

    Fix mongoose options

--- server.js
+++ server.js
@@ -4,13 +4,7 @@ const mongoose = require("mongoose");
 
 mongoose.set("bufferCommands", false);
 
-mongoose.connect(
-    process.env.MONGO_URI,
-    {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    }
-)
+mongoose.connect(process.env.MONGO_URI)
 .then(() => {
     console.log("🔥 MongoDB conectado");
 })
