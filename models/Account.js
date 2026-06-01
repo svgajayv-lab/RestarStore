edfaed817e5f0247c347fb29759c69dec7461e24
Natalio <svgajayv@gmail.com>
2026-05-31 19:16:56 -0500

    Agregar ownerId a cuentas

--- models/Account.js
+++ models/Account.js
@@ -1,7 +1,10 @@
-const mongoose = require("mongoose");
-
 const AccountSchema =
 new mongoose.Schema({
+
+    ownerId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    },
 
     plataforma: String,
 
